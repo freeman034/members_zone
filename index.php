449cf2583a94462e7afbbbcafa29d60fc3d4c06e
freeman034 <freeman034@hotmail.com>
2019-02-19 21:25:32 +0100

    Commit 6

--- index.php
+++ index.php
@@ -1,6 +1,32 @@
 <?php
-require('model/model.php');
+require('controller/frontend.php');
 
-$posts = getPosts();
-
-require('view/listPostsView.php');
+if (isset($_GET['action'])) {
+    if ($_GET['action'] == 'listPosts') {
+        listPosts();
+    }
+    elseif ($_GET['action'] == 'post') {
+        if (isset($_GET['id']) && $_GET['id'] > 0) {
+            post();
+        }
+        else {
+            echo 'Erreur : aucun identifiant de billet envoyé';
+        }
+    }
+    elseif ($_GET['action'] == 'addComment') {
+        if (isset($_GET['id']) && $_GET['id'] > 0) {
+            if (!empty($_POST['author']) && !empty($_POST['comment'])) {
+                addComment($_GET['id'], $_POST['author'], $_POST['comment']);
+            }
+            else {
+                echo 'Erreur : tous les champs ne sont pas remplis !';
+            }
+        }
+        else {
+            echo 'Erreur : aucun identifiant de billet envoyé';
+        }
+    }
+}
+else {
+    listPosts();
+}
